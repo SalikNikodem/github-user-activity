a2a15ddca2b161902ac5214b8a9bcbc567890876
SalikNikodem <saliknikodem@gmail.com>
2026-03-17 19:18:14 +0100

    Filtering by Event added

--- functions.py
+++ functions.py
@@ -3,32 +3,42 @@ from urllib.request import Request, urlopen
 from urllib.error import HTTPError, URLError
 
 
-def github_activity(user, timeout=5):
+def github_activity(user, key = None,timeout=5):
     url = f"https://api.github.com/users/{user}/events"
     headers = {'User-Agent': 'MonitorApp/1.0'}
-
+    found = False
     try:
         request = Request(url, headers=headers)
         with urlopen(request, timeout=timeout) as answer:
             events = json.loads(answer.read())
+
+            if not events:
+                print(f"No activity from user {user}")
+
             for event in events:
                 event_type = event.get("type")
                 repository_name = event.get("repo", {}).get("name")
-
+                if key and key != event_type:
+                    continue
                 match event_type:
                     case "PushEvent":
                         print(f"Pushed changes to {repository_name}")
-
+                        found = True
                     case "IssuesEvent":
                         issue = event.get("payload", {}).get("action")
                         print(f"{issue} a new issue in {repository_name}")
+                        found = True
 
                     case "WatchEvent":
                         print(f"Starred {repository_name}")
+                        found = True
 
                     case "CreateEvent":
                         ref_type = event.get("payload", {}).get("ref_type")
                         print(f"Created a new {ref_type} in {repository_name}")
+                        found = True
+            if not found:
+                print(f"No activities found for type {key}")
         return ''
 
     except HTTPError as e:
--- user_activity.py
+++ user_activity.py
@@ -2,7 +2,15 @@ from functions import github_activity
 import sys
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("Run: python user_activity.py [user]")
+        print("Run: python user_activity.py [user] [event](optional)")
+        sys.exit(1)
+    user = sys.argv[1]
+    events = ["PushEvent", "IssuesEvent", "WatchEvent", "CreateEvent"]
+
+    key = sys.argv[2] if len(sys.argv) > 2 else None
+
+    if key and key not in events:
+        print(f"Filter events use from list: {events}")
         sys.exit(1)
 
-    print(github_activity(sys.argv[1]))
+    github_activity(user, key=key)
