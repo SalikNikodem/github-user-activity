e9d3c59a1cf09907d4964f1ccb7d6f73ddc1a503
SalikNikodem <saliknikodem@gmail.com>
2026-03-18 21:06:56 +0100

    added caching the fetched data to imporove performance

--- functions.py
+++ functions.py
@@ -2,49 +2,26 @@ import json
 from urllib.request import Request, urlopen
 from urllib.error import HTTPError, URLError
 from datetime import datetime
-
+import time
+from pathlib import Path
 def github_activity(user, key = None,timeout=5):
+    cache_f = Path(f"{user}_cache.json")
+    found = False
+    if cache_f.exists() and (time.time() - cache_f.stat().st_mtime < 1200):
+        with open(cache_f, "r") as ff:
+            events = json.load(ff)
+            match(events, key=key, found=found, user=user)
+            return ''
+
     url = f"https://api.github.com/users/{user}/events"
     headers = {'User-Agent': 'MonitorApp/1.0'}
-    found = False
     try:
         request = Request(url, headers=headers)
         with urlopen(request, timeout=timeout) as answer:
             events = json.loads(answer.read())
-            print(json.dumps(events, indent=4))
-            if not events:
-                print(f"No activity from user {user}")
-
-            for event in events:
-                event_type = event.get("type")
-                repository_name = event.get("repo", {}).get("name")
-                raw_date = event.get("created_at").replace("Z","")
-
-                #ISO 8601 type convert
-                date = datetime.fromisoformat(raw_date)
-
-                if key and key != event_type:
-                    continue
-                match event_type:
-                    case "PushEvent":
-                        print(f"{date}: Pushed changes to {repository_name}")
-                        found = True
-                    case "IssuesEvent":
-                        issue = event.get("payload", {}).get("action")
-                        print(f"{date}: {issue} a new issue in {repository_name}")
-                        found = True
-
-                    case "WatchEvent":
-                        print(f"{date}: Starred {repository_name}")
-                        found = True
-
-                    case "CreateEvent":
-                        ref_type = event.get("payload", {}).get("ref_type")
-                        print(f"{date}: Created a new {ref_type} in {repository_name}")
-                        found = True
-            if not found and key:
-                print(f"No activities found for type {key}")
-        return ''
+        with open(cache_f, "w") as ff:
+            json.dump(events, ff)
+        match(events,key=key, found=found,user=user)
 
     except HTTPError as e:
         code = e.code
@@ -58,4 +35,37 @@ def github_activity(user, key = None,timeout=5):
     except Exception as e:
         return f'Error: {e}'
 
-#need to add more events
+def match(events, key, found, user):
+    if not events:
+        print(f"No activity from user {user}")
+
+    for event in events:
+        event_type = event.get("type")
+        repository_name = event.get("repo", {}).get("name")
+        raw_date = event.get("created_at").replace("Z", "")
+
+        # ISO 8601 type convert
+        date = datetime.fromisoformat(raw_date)
+
+        if key and key != event_type:
+            continue
+        match event_type:
+            case "PushEvent":
+                print(f"{date}: Pushed changes to {repository_name}")
+                found = True
+            case "IssuesEvent":
+                issue = event.get("payload", {}).get("action")
+                print(f"{date}: {issue} a new issue in {repository_name}")
+                found = True
+
+            case "WatchEvent":
+                print(f"{date}: Starred {repository_name}")
+                found = True
+
+            case "CreateEvent":
+                ref_type = event.get("payload", {}).get("ref_type")
+                print(f"{date}: Created a new {ref_type} in {repository_name}")
+                found = True
+    if not found and key:
+        print(f"No activities found for type {key}")
+    return ''
